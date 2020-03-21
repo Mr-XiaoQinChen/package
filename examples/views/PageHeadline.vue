c77ad6909bd6d727d21f23dff6892ed7985837b9
xiao chen <xiaoqinchen0827@163.com>
2020-03-21 21:00:56 +0800

    开发headline组件-设置大小、图标、内容

--- examples/views/PageHeadline.vue
+++ examples/views/PageHeadline.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-     <h1>测试my-headline组件</h1>
-     <my-headline></my-headline>
+    <h1>测试my-headline组件</h1>
+    <my-headline :level="1" icon="user" size="Huge">
+      标题
+      <div slot="sub">
+        子标题子标题子标题子标题
+      </div>
+    </my-headline>
   </div>
 </template>
 
@@ -9,7 +14,6 @@
 export default {
   name: 'page-headline'
 }
-
 </script>
 <style scoped lang='less'>
 </style>
--- packages/headline/headline.vue
+++ packages/headline/headline.vue
@@ -1,16 +1,44 @@
 <template>
-  <h2 class="ui header">
-    <i class="settings icon"></i>
-    <div class="content">
-      Account Settings
-      <div class="sub header">Manage your preferences</div>
-    </div>
-  </h2>
+  <div>
+    <h1 class="ui header" :class="size">
+      <i v-if="icon" class="icon" :class="icon"></i>
+      <div class="content">
+        <slot></slot>
+        <!-- <slot name="sub"></slot> -->
+        <div class="sub header"><slot name="sub"></slot></div>
+      </div>
+    </h1>
+    <!-- <h2 v-if="level===2" class="ui header" :class="size">
+      <i v-if="icon" class="icon" :class="icon"></i>
+      <div class="content">
+        <slot></slot>
+        <div class="sub header"><slot name="sub"></slot></div>
+      </div>
+    </h2> -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Myheadline'
+  name: 'Myheadline',
+  // 定义属性来收集用户的输入
+  props: {
+    level: {
+      type: Number, // h1 h2 h3 ...
+      default: 1, // 默认为1
+      requried: false
+    },
+    icon: {
+      type: String, // 图标
+      default: '', // 默认为空
+      requried: false
+    },
+    size: {
+      type: String, // 大小
+      default: 'small', // 默认为small
+      requried: false
+    }
+  }
 }
 </script>
 <style scoped lang='less'>
