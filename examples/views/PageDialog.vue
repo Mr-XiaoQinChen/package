c83896f0579123343308bea3d32d625e10af0606
xiao chen <xiaoqinchen0827@163.com>
2020-03-22 14:25:08 +0800

    开发Dialog弹窗组件

---
+++ examples/views/PageDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <h1>测试my-dialog</h1>
+    <button type="primary" @click="isShow = !isShow">切换显示</button>
+    <my-dialog v-model="isShow">
+
+    </my-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageDialog',
+  data () {
+    return {
+      isShow: false
+    }
+  }
+}
+</script>
+
+<style lang="" scoped>
+
+</style>
--- packages/index.js
+++ packages/index.js
@@ -5,6 +5,7 @@ import MyButton from './button/button.vue'
 import MyDivider from './divider/divider.vue'
 import MySlider from './slider/slider.vue'
 import MyHeadline from './headline/headline.vue'
+import MyDialog from './dialog/dialog.vue'
 // 导出 组件
 export default {
   install (Vue) {
@@ -12,5 +13,6 @@ export default {
     Vue.component('MyDivider', MyDivider)
     Vue.component('MySlider', MySlider)
     Vue.component('MyHeadline', MyHeadline)
+    Vue.component('MyDialog', MyDialog)
   }
 }
